cc8cb1488c8ba75a073b072a1a841774f558a064
Supreet Pal Singh <supreetpal@gmail.com>
2016-03-01 02:02:05 +0530

    Add placeholder folders.

---
+++ examples/data.js
@@ -0,0 +1,19 @@
+let domList = [{
+  id: 1,
+  name: "John Doe",
+  img: "http://some.image/image1.jpg",
+}, {
+  id: 2,
+  name: "Pauline Halen",
+  img: "http://some.image/image2.jpg",
+}, {
+  id: 3,
+  name: "Matt Hill",
+  img: "http://some.image/image3.jpg",
+}, {
+  id: 400,
+  name: "Jessica Bedford",
+  img: "http://some.image/image400.jpg",
+}, ];
+
+export default domList;
