7a954c0775e79f5f519312aa194d0f2a9265438f
Supreet Pal Singh <supreetpal@gmail.com>
2016-03-07 03:06:04 +0530

    Add uglify to production webpack config for example.

--- example/webpack.prod.config.js
+++ example/webpack.prod.config.js
@@ -1,9 +1,14 @@
+var webpack = require("webpack");
+
 module.exports = {
   entry: './src/controller',
   output: {
     path: __dirname + "/dist",
     filename: "bundle.js"
   },
+  plugins: [
+    new webpack.optimize.UglifyJsPlugin({ minimize: true })
+  ],
   module: {
     loaders: [
       { test: /\.js?$/, loader: "babel" }
