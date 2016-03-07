7277f7d73bd9f0e33d63a3187bdbd7f74b75648d
Supreet Pal Singh <supreetpal@gmail.com>
2016-03-07 12:49:40 +0530

    Add comments to InfiniteDivs class.

--- src/infinitedivs.js
+++ src/infinitedivs.js
@@ -1,11 +1,28 @@
+/* Infinite Divs Class
+ * takes an input config
+ * loads the first view + buffer
+ * on inititalization.
+ * exposes a viewDoctor fn
+ * which can be attached to an eventListener
+ * to maintain and update elements in the DOM.
+ * @param {Object}
+ */
 class InfiniteDivs {
 
+  /* initialize the class with the config 
+   * and generate the first view with buffer
+   */
   constructor(config) {
     this.config = config;
     this.lastNodeIndex = 0;
     this.generateView();
   }
 
+  /* iterate over the data
+   * and create the dom elements
+   * using the divGenerator function.
+   * append these nodes to rootElement
+   */
   appendNodes(nodeList) {
     let config = this.config,
       parent = config.rootElement,
@@ -19,6 +36,15 @@ class InfiniteDivs {
     parent.appendChild(docfrag);
   }
 
+  /* check if there is data
+   * which has not been consumed.
+   * slice the data based on the
+   * last consumed data index
+   * and the number of dom nodes 
+   * required by the view + buffer.
+   * call the appendNodes fn.
+   * update the last consumed data index.
+   */
   generateView() {
     let config = this.config,
       bufferMultiplier = config.bufferMultiplier,
@@ -37,6 +63,13 @@ class InfiniteDivs {
     }
   }
 
+  /* check if the number of nodes
+   * required in the view buffer
+   * is lower than the config value.
+   * nodes in the view buffer will be 
+   * 2 times the nodes in view if
+   * bufferMultiplier is 3.
+   */
   viewDoctor() {
     let config = this.config,
       parentNode = config.rootElement;
